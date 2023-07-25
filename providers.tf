b781f1a28f05101813936a1f2db8bfc728834016
Lyon Till <lyon.till@microsoft.com>
2023-07-26 00:28:48 +0100

    feat: add helm releases

--- providers.tf
+++ providers.tf
@@ -4,6 +4,9 @@ terraform {
     azurerm = {
       source = "hashicorp/azurerm"
     }
+    helm = {
+      source = "hashicorp/helm"
+    }
   }
 }
 
@@ -12,5 +15,14 @@ provider "azurerm" {
     resource_group {
       prevent_deletion_if_contains_resources = false
     }
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.cluster.kube_config.0.host
+    client_certificate     = base64decode(module.cluster.kube_config.0.client_certificate)
+    client_key             = base64decode(module.cluster.kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(module.cluster.kube_config.0.cluster_ca_certificate)
   }
 }
